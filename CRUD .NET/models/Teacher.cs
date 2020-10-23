18b54f13a94d2a8cdbcb77b939b7e009a3471c99
Hassan <hassanalihazaraa@gmail.com>
2020-10-23 21:36:08 +0200

    teacher and student model

---
+++ CRUD .NET/models/Teacher.cs
@@ -0,0 +1,10 @@
+namespace DefaultNamespace
+{
+    public class Teacher
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+    }
+}
